4051bb2c8280edbebdfe4c96fe8f0c723f56a16b
Allan Wikström <allan.wikstrom@svenskaspel.se>
2026-01-27 21:45:01 +0100

    Enable source maps for easier browser debugging

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -4,6 +4,9 @@ import preact from '@preact/preset-vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [preact()],
+  build: {
+    sourcemap: true,
+  },
   server: {
     proxy: {
       '/api': {
