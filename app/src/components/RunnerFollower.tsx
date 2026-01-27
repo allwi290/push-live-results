e5777435023649be6738895594df596a892bb1ee
Allan Wikström <allan.wikstrom@svenskaspel.se>
2026-01-27 21:13:32 +0100

    Fix for start time

--- app/src/components/RunnerFollower.tsx
+++ app/src/components/RunnerFollower.tsx
@@ -20,10 +20,10 @@ type RunnerFollowerProps = {
   onSortDirectionToggle: () => void
 }
 
-const formatStartTime = (startMs: number): string => {
-  const hours = Math.floor(startMs / 3600000)
-  const minutes = Math.floor((startMs % 3600000) / 60000)
-  const seconds = Math.floor((startMs % 60000) / 1000)
+const formatStartTime = (startHundredsOfSeconds: number): string => {
+  const hours = Math.floor(startHundredsOfSeconds / 360000)
+  const minutes = Math.floor((startHundredsOfSeconds % 360000) / 6000)
+  const seconds = Math.floor((startHundredsOfSeconds % 6000) / 100)
   return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
 }
 
